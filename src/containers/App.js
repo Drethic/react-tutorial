47f9fdbfbb3e57839f9b2b1f5ff90be7e23b6499
Drethic <drethic@msn.com>
2017-11-02 13:31:54 -0400

    Code Clean-up * Fixed issues with JsFormat vs JSX

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,6 +11,8 @@ import {
 import Char from '../components/Char/Char';
 import Cockpit from '../components/Cockpit/Cockpit';
 
+//Performance Gains with PureComponents
+
 class App extends Component {
     constructor(props) {
         super(props); //must be called since we are overriding Reacts default constructor
@@ -236,152 +238,112 @@ class App extends Component {
         }
 
         if (this.state.showPersons) {
-            persons = ( < Persons persons = {
-                    this.state.persons
-                }
-                changeName = {
-                    this.stateModifyPersonNameHandler
-                }
-                changeAge = {
-                    this.stateModifyPersonAgeHandler
-                }
+            persons = (
+                <Persons
+                persons = {this.state.persons}
+                changeName = {this.stateModifyPersonNameHandler}
+                changeAge = {this.stateModifyPersonAgeHandler}
                 />
             );
         }
 
         if (this.state.showOutput) {
-            output = ( < div > {
-                    this.state.names.map((name, key) => ( < UserOutput name = {
-                            name
-                        }
-                        key = {
-                            key
-                        }
-                        change = {
-                            this.stateModifyHandler.bind(this, key)
-                        }
+            output = (
+                <div>
+                    {this.state.names.map((name, key) => (
+                        <UserOutput
+                            name = {name}
+                            key = {key}
+                            change = {this.stateModifyHandler.bind(this, key)}
                         />
-                    ))
-                } < /div>);
-            }
-
-            if (this.state.showAge) {
-                age = ( < div > < p > States
-                    for Ages array: {
-                        this.state.ages.join(', ')
-                    } < /p></div >
-                );
-            }
-
-            if (this.state.showTask1) {
-                task1 = ( < div >
-                    < ol >
-                    < li > Create TWO new components: UserInput and UserOutput < /li> < li > UserInput should hold an input element, UserOutput two paragraphs < /li > < li > Output multiple UserOutput components in the App component(any paragraph texts of your choice) < /li> < li > Pass a username(of your choice) to UserOutput via props and display it there < /li > < li > Add state to the App component( => the username) and pass the username to the UserOutput component < /li> < li > Add a method to manipulate the state( => an event - handler method) < /li > < li > Pass the event - handler method reference to the UserInput component and bind it to the input - change event < /li> < li > Ensure that the new input entered by the user overwrites the old username passed to UserOutput < /li > < li > Add two - way - binding to your input( in UserInput) to also display the starting username < /li> < li > Add styling of your choice to your components /
-                    elements in the components - both with inline styles and stylesheets < /li> < /ol > < button className = "btn"
-                    onClick = {
-                        this.toggleOutput
-                    } > Toggle Output < /button> {
-                    output
-                } < button className = "btn"
-                onClick = {
-                    this.toggleAge
-                } > Toggle Age < /button> {
-                age
-            } < button className = "btn"
-            onClick = {
-                this.togglePersons
-            } > Toggle Persons < /button> {
-            persons
-        } < /div>
-    );
-}
-
-if (this.state.showTask2) {
-    task2 = ( < div >
-            < ol >
-            < li > Create an input field( in App component) with a change listener which outputs the length of the entered text below it(e.g.in a paragraph). < /li> < li > Create a new component( => ValidationComponent) which receives the text length as a prop < /li > < li > Inside the ValidationComponent, either output "Text too short"
-            or "Text long enough"
-            depending on the text length(e.g.take 5 as a minimum length) < /li> < li > Create another component( => CharComponent) and style it as an inline box( => display: inline - block, padding: 16 px, text - align: center, margin: 16 px, border: 1 px solid black). < /li > < li > Render a list of CharComponents where each CharComponent receives a different letter of the entered text( in the initial input field) as a prop. < /li> < li > When you click a CharComponent, it should be removed from the entered text. < /li > < /ol> < p > Hint: Keep in mind that JavaScript strings are basically arrays! < /p > < input type = "text"
-            onChange = {
-                this.changeHandler
-            }
-            value = {
-                this.state.text
-            }
-            /> < p > Text Length: {
-            this.state.text.length
-        } < /p><Length length={this.state.text.length} / >
-        < Char text = {
-            this.state.text
+                    ))}
+                </div>
+            );
         }
-    delete = {
-        this.deleteLetterHandler
-    }
-    /> < /div >
-);
-}
 
-if (this.state.showMainApp) {
-    appButtonStyle.backgroundColor = 'red';
-    appButtonStyle[':hover'].backgroundColor = 'salmon';
-    appButtonStyle[':hover'].color = 'black';
-    const assignedClasses = [];
-    if (this.state.appPersons.length <= 2) {
-        assignedClasses.push('red');
-    }
-    if (this.state.appPersons.length <= 1) {
-        assignedClasses.push('bold');
-    }
-    mainApp = ( < div >
-        < p className = {
-            assignedClasses.join(' ')
-        } > This is really working! < /p> < Persons persons = {
-        this.state.appPersons
-    }
-    clicked = {
-        this.deletePersonHandler
-    }
-    changeName = {
-        this.stateModifyAppPersonNameHandler
-    }
-    changeAge = {
-        this.stateModifyAppPersonAgeHandler
-    }
-    /> < /div >
-);
-}
+        if (this.state.showAge) {
+            age = ( < div > < p > States
+                for Ages array: {
+                    this.state.ages.join(', ')
+                } < /p></div >
+            );
+        }
 
-return ( < div className = "App" >
-    < Cockpit appTitle = {
-        this.props.title
+        if (this.state.showTask1) {
+            task1 = (
+                <div>
+                    <ol>
+                        <li>Create TWO new components: UserInput and UserOutput </li>
+                        <li>UserInput should hold an input element, UserOutput two paragraphs</li>
+                        <li>Output multiple UserOutput components in the App component(any paragraph texts of your choice)</li>
+                        <li>Pass a username(of your choice) to UserOutput via props and display it there</li>
+                        <li>Add state to the App component( => the username) and pass the username to the UserOutput component</li>
+                        <li>Add a method to manipulate the state( => an event - handler method)</li>
+                        <li>Pass the event - handler method reference to the UserInput component and bind it to the input - change event</li>
+                        <li>Ensure that the new input entered by the user overwrites the old username passed to UserOutput</li>
+                        <li>Add two - way - binding to your input( in UserInput) to also display the starting username</li>
+                        <li> Add styling of your choice to your components/elements in the components - both with inline styles and stylesheets</li>
+                    </ol>
+                    <button className = "btn" onClick = {this.toggleOutput}>Toggle Output</button>
+                    {output}
+                    <button className = "btn" onClick = {this.toggleAge}>Toggle Age</button>
+                    {age}
+                    <button className = "btn" onClick = {this.togglePersons}>Toggle Persons</button>
+                    {persons}
+                </div>
+            );
+        }
+
+        if (this.state.showTask2) {
+            task2 = (
+                <div>
+                    <ol>
+                        <li>Create an input field( in App component) with a change listener which outputs the length of the entered text below it(e.g.in a paragraph).</li>
+                        <li>Create a new component( => ValidationComponent) which receives the text length as a prop</li>
+                        <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length(e.g.take 5 as a minimum length)</li>
+                        <li>Create another component( => CharComponent) and style it as an inline box( => display: inline - block, padding: 16 px, text - align: center, margin: 16 px, border: 1 px solid black).</li>
+                        <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text( in the initial input field) as a prop.</li>
+                        <li> When you click a CharComponent, it should be removed from the entered text.</li>
+                    </ol>
+                    <p>Hint: Keep in mind that JavaScript strings are basically arrays!</p>
+                    <input type = "text" onChange = {this.changeHandler} value = {this.state.text} />
+                    <p> Text Length: {this.state.text.length}</p><Length length={this.state.text.length} />
+                    <Char text = {this.state.text} delete = {this.deleteLetterHandler} />
+                </div>
+            );
+        }
+
+        if (this.state.showMainApp) {
+            appButtonStyle.backgroundColor = 'red';
+            appButtonStyle[':hover'].backgroundColor = 'salmon';
+            appButtonStyle[':hover'].color = 'black';
+            const assignedClasses = [];
+            if (this.state.appPersons.length <= 2) {
+                assignedClasses.push('red');
+            }
+            if (this.state.appPersons.length <= 1) {
+                assignedClasses.push('bold');
+            }
+            mainApp = (
+                <div>
+                    <p className = {assignedClasses.join(' ')} > This is really working! </p>
+                    <Persons persons = {this.state.appPersons} clicked = {this.deletePersonHandler} changeName = {this.stateModifyAppPersonNameHandler} changeAge = {this.stateModifyAppPersonAgeHandler} />
+                </div>
+            );
+        }
+
+        return (
+            <div className = "App">
+                <Cockpit appTitle = {this.props.title} />
+                <button style = {buttonStyle} key = "task1" onClick = {this.toggleTask1}>Toggle Task 1</button>
+                {task1}
+                <button style = {buttonStyle} key = "task2" onClick = {this.toggleTask2}>Toggle Task 2</button>
+                {task2}
+                <button style = {appButtonStyle} key = "mainApp" onClick = {this.toggleMainApp}>Toggle Main App</button>
+                {mainApp}
+            </div>
+        );
     }
-    /> < button style = {
-    buttonStyle
-}
-key = "task1"
-onClick = {
-    this.toggleTask1
-} > Toggle Task 1 < /button> {
-task1
-} < button style = {
-    buttonStyle
-}
-key = "task2"
-onClick = {
-    this.toggleTask2
-} > Toggle Task 2 < /button> {
-task2
-} < button style = {
-    appButtonStyle
-}
-key = "mainApp"
-onClick = {
-this.toggleMainApp
-} > Toggle Main App < /button> {
-mainApp
-} < /div>
-);
-}
 }
 
 export default Radium(App);
