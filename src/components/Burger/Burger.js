65a4df0b0ea193de9a1c50581a35925d3e5fcb9f
Drethic <drethic@msn.com>
2017-11-03 15:18:03 -0400

    Calculating the Ingredient Sum Dynamically * Setup a reduce function in Burger * Returned default text if no ingredients in state * Updated state in BurgerBuilder to be empty * Code up to date with lecture 113

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,12 +3,16 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    const transformedIngredients = Object.keys(props.ingredients)
+    let transformedIngredients = Object.keys(props.ingredients)
         .map(igKey => {
             return [...Array(props.ingredients[igKey])].map((_, i) => (
                 <BurgerIngredient key={igKey + i} type={igKey} />
             ));
-        });
+        })
+        .reduce((arr, el) => (arr.concat(el)), []);
+        if (0 === transformedIngredients.length) {
+            transformedIngredients = <p>Please start adding ingredients!</p>;
+        }
     return(
         <div className={classes.Burger}>
             <BurgerIngredient type="bread-top" />
